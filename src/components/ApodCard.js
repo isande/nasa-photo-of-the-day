a1329f4794889876944a432108e342336f2b9810
Peggy Wollenhaupt <color.ui>
2020-02-04 11:52:17 -0700

    MVP complete + works with videos

--- src/components/ApodCard.js
+++ src/components/ApodCard.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./ApodCard.css";
 
-function ApodCard() {
+const ApodCard = (props) => {
+    if (!props.url) return <h3>Loading...</h3>;
+
+    const isImage = props.media === 'image';
+
     return (
         <div>
-            ApodCard component
+            <h1>{props.title}</h1>
+            <h2>{props.copyright}</h2>
+            <p>{props.date}</p>
+            {isImage && <img src={props.url} alt="NASA APOD"></img>}
+            {!isImage && <iframe title="Apod Video" src={props.url}></iframe>}
+            <p class="caption">{props.caption}</p>
         </div>
     );
 }
